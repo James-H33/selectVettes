0c9b25f8eb140d8a6e3156e8103e395618f10d10
James Hall <llahwj33@gmail.com>
2016-04-14 19:45:08 -0400

    Fully Styled the index page

--- public/js/functions.js
+++ public/js/functions.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
     $('.nav-icon').toggleClass('icon-slide');
   })
 
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 300) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+    });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},500);
+    return false;
+  });
+
 })
