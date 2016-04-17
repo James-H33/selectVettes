c1f983f45481d30ba3e730be8a0d1ba7b59b64d5
James Hall <llahwj33@gmail.com>
2016-04-16 22:20:55 -0400

    Added additional functionality and effects.

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,15 +4,26 @@ $(document).ready(function() {
     $('.nav-icon span').toggleClass('open');
     $('.nav-wrapper').toggleClass('nav-slide');
     $('.nav-icon').toggleClass('icon-slide');
+    $('.nav-display').toggleClass('nav-display-active');
   })
 
   //Check to see if the window is top if not then display button
   $(window).scroll(function(){
+
+    // Var for calculating window distance from top
+    var scrollCheck = $(window).scrollTop();
+
     if ($(this).scrollTop() > 300) {
       $('.scrollToTop').fadeIn();
     } else {
       $('.scrollToTop').fadeOut();
     }
+
+    // Scroll Index headings
+    $('.index-intro-display').css({
+      'transform' : 'translateY(0' + scrollCheck * 0.5 + 'px)'
+    })
+
     });
 
   //Click event to scroll to top
