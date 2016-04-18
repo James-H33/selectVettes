d3791c6a714d030cf39b1b404c94499f0a18e41b
James Hall <llahwj33@gmail.com>
2016-04-17 22:16:16 -0400

    Added Database functionality. Using MLab with heroku.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app         = express();
 var mongoose    = require('mongoose');
 var bodyParser  = require('body-parser');
 var Inventory   = require('./models/inventoryModel');
+var seedDB      = require('./seed');
 
 // Ports Control
 var port = process.env.PORT || 3000;
@@ -11,10 +12,15 @@ var portIP = process.env.IP;
 // Routes
 var inventoryRoutes = require('./routes/inventoryRoutes');
 
-
+//Mongo Database
+mongoose.connect('mongodb://retsbud:String33@ds011241.mlab.com:11241/select_vettes');
+// mongoose.connect('mongodb://localhost/select_vettes');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+
+// Seed Database
+seedDB();
 
 app.get('/', function(req, res) {
     res.redirect('/home');
